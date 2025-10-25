4ad3d71f91dfafc5703477d9eb1de1e80954f7e5
Mir Ilham <mhmilham3@gmail.com>
2025-10-25 19:33:20 +0600

    Add custom error handler middleware. Add number update functionality. Refactor delete, getPersonById and getInfo to use db connection.

--- index.js
+++ index.js
@@ -21,33 +21,39 @@ const format = ':method :url :status :res[content-length] - :response-time ms :p
 
 app.use(morgan(format));
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
   const now = new Date()
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people</p>
-     <p>${now.toString()}</p>
-    `
-  )
+  Person.find({})
+    .then(persons => {
+      return response.send(
+        `<p>Phonebook has info for ${persons.length} people</p>
+        <p>${now.toString()}</p>
+        `
+      )
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(persons => {
     response.json(persons)
   })
+  .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-  
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const name = request.body.name
   const number = request.body.number
 
@@ -77,19 +83,50 @@ app.post('/api/persons', (request, response) => {
     number: number,
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const match = persons.find(person => person.id === id)
-  persons = persons.filter(person => person.id !== id)
+app.put('/api/persons/:id', (request, response, next) => {
+  const number = request.body.number
 
-  // response.status(204).end()
-  response.json(match)
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.number = number
+
+      return person.save().then((updatedPerson => {
+        response.json(updatedPerson)
+      }))
+    })
+    .catch(error => next(error))
 })
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.json(result)
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
